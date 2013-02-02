be2ead65d1eef294820a49a191a9d8838810728f
lamberbe <lamberbe@onid.oregonstate.edu>
2013-02-02 11:09:47 -0800

    moved files to correct folders

--- milestone2/stutest.py
+++ milestone2/stutest.py
@@ -1,1 +1,29 @@
+import re
+import string
+
+def isOperator(token):
+	operators = ['+', '-', '*', '/', '%', '^', 'and', 'or', 'not', 'iff']
+	return token in operators
+
+def isBoolean(token):
+	bools = ['true', 'false']
+	return token in bools
+
+def isInteger(token):
+	regex = re.compile("^[1-9]\d*$")
+	return regex.match(token)
+
+def isReal(token):
 #TODO: Implement
+
+def isString(token):
+	regex = re.compile("^\"\"$")
+	return regex.match(token)
+
+def isType(token):
+	types = ['bool', 'int', 'real', 'string']
+	return token in types
+
+def isStatement(token):
+	statements = ['println', 'if', 'while', 'let', 'assign']
+	return token in statements
